f33c3442b8ea710b6c1d2127a69bdb787c9c23da
jonathon <jonathonwhite@protonmail.com>
2021-03-22 12:01:16 -0400

    spell checker for latex

---
+++ bin/spellcheck.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# spell checking for latex files
+
+# find dictionary of added words in home dir
+# ~/.aspell.en.pws
+
+
+# a -> add to dictionary
+# usage:
+# aspell -t -c file.tex
+filename=$1
+aspell -t -c $filename
+
+
