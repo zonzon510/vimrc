4daf459a9baf7181049c3d4c3da30d5a46a15c6d
jonathon white <jonathonwhite@protonmail.com>
2021-07-12 11:32:09 -0400

    install java

--- install_sdk.sh
+++ install_sdk.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# install java
+sudo apt install openjdk-13-jdk-headless
+
 sudo apt install unzip zip
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk install springboot
 spring version
+
